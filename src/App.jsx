250144e564658769bb8facbb550350e6b518010c
Asad9340 <asadulimran1999@gmail.com>
2024-03-07 23:33:46 +0600

    added all functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,38 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  let localTime = new Date().toLocaleTimeString();
-  const [time, setTime] = useState(localTime);
-  const updateTime = () => {
-    localTime = new Date().toLocaleTimeString();
-    setTime(localTime);
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [isRunning, setIsRunning] = useState(true);
+
+  useEffect(() => {
+    let intervalId;
+    if (isRunning) {
+      intervalId = setInterval(() => {
+        setTime(new Date().toLocaleTimeString());
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [isRunning]);
+
+  const toggleClock = () => {
+    setIsRunning(!isRunning);
   };
-  const stopTimeInterval = setInterval(updateTime, 1000)
-  const stopTime = () => {
-    clearInterval(stopTimeInterval);
-  }
+
   return (
     <>
       <div className="text-center w-full h-screen flex flex-col justify-center items-center bg-gray-400">
-        <h1 className="text-9xl font-extrabold text-[#0b0920]">{time}</h1>
+        <h1 className="text-9xl font-extrabold text-[#0b0920]">
+          {time}
+        </h1>
         <button
-          onClick={stopTime}
+          onClick={toggleClock}
           className="border px-6 py-3 text-xl font-semibold rounded-xl mt-6 hover:bg-gray-900 hover:text-white"
         >
-          Stop
+          {isRunning ? 'Stop' : 'Start'}
         </button>
       </div>
     </>
